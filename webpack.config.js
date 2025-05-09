a5842f2b0a01416b6d40bb76a7c314144529ca97
Linas Zilinskas <linaszilinskasas@gmail.com>
2025-05-09 14:09:00 +0300

    empty popup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
 module.exports = {
     mode: "development",
-    entry: "./src/test.tsx",
+    entry: {
+        popup: path.resolve("src/popup/popup.tsx"),
+    },
     module: {
         rules: [
             {
@@ -10,6 +15,22 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve("src/assets/manifest.json"), to: path.resolve(__dirname, "dist") },
+                {
+                    from: path.resolve("src/assets/icon.png"),
+                    to: path.resolve(__dirname, "dist"),
+                },
+            ],
+        }),
+        new HtmlPlugin({
+            title: "ReactJS Boilerplate",
+            filename: "popup.html",
+        }),
+    ],
+
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
     },
