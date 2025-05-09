0dca05441aa930961479c3f49527a1711e4f77e0
user-blank1 <linaszilinskasas@gmail.com>
2025-05-09 23:10:31 +0300

    tailwind set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const HtmlPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+
+const tailwindcss = require("tailwindcss");
+
 module.exports = {
     mode: "development",
     devtool: "cheap-module-source-map",
@@ -14,14 +18,24 @@ module.exports = {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
             },
+            {
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                    },
+                ],
+                test: /\.css$/i,
+            },
         ],
     },
     plugins: [
         new CopyPlugin({
             patterns: [
-                { from: path.resolve("src/assets/manifest.json"), to: path.resolve(__dirname, "dist") },
+                { from: path.resolve("src/static/manifest.json"), to: path.resolve(__dirname, "dist") },
                 {
-                    from: path.resolve("src/assets/icon.png"),
+                    from: path.resolve("src/static/icon.png"),
                     to: path.resolve(__dirname, "dist"),
                 },
             ],
